75527a32988716c970507ffd2e32c7027cef4c43
Janota1 <janetjanota@gmail.com>
2023-02-16 17:17:24 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)", sizeof(chart));
+printf("size of int: %lu byte(s)", sizeof(int));
+printf("size of a long int: %lu byte(s)", sizeof(long int));
+printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("size of float: %lu byte(s)", siz0f(float));
+return (0);
+
+}
