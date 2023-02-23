46c52dcf250d66c0a2b4b8216acdcc7983a2cc38
Janota1 <janetjanota@gmail.com>
2023-02-23 14:28:23 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,6 @@
 
 #include <math.h>
 
-
 /**
 
  * main - finds and prints the largest prime factor of the number 612852475143
@@ -24,11 +23,9 @@ int main(void)
 
  long int i;
 
-
  n = 612852475143;
 
  max = -1;
-
 
  while (n % 2 == 0)
 
@@ -39,7 +36,6 @@ int main(void)
  n /= 2;
 
  }
-
 
  for (i = 3; i <= sqrt(n); i = i + 2)
 
@@ -57,14 +53,11 @@ int main(void)
 
  }
 
-
  if (n > 2)
 
  max = n;
 
-
  printf("%ld\n", max);
-
 
  return (0);
 
