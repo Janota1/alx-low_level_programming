1e59089c3ec2324902bd733b7ca91a52096ee180
Janota1 <janetjanota@gmail.com>
2023-03-01 08:22:14 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,11 @@
 
 int main(void)
 {
-
 	printf("Size of a char: %d byte(s)\n", sizeof(chart));
-
 	printf("size of int: %d byte(s)\n", sizeof(int));
-
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
-
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-
 	printf("size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
